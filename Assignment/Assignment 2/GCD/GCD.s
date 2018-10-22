a5819767f074d049b7858551039a8ebff9ba271a
ayushigupta0317 <44375350+ayushigupta0317@users.noreply.github.com>
2018-10-22 23:26:14 +0530

    Update GCD.s

--- Assignment/Assignment 2/GCD/GCD.s
+++ Assignment/Assignment 2/GCD/GCD.s
@@ -1,1 +1,18 @@
+     AREA     appcode, CODE, READONLY
+     export __main	 
+	 ENTRY 
+__main  function
+	          MOV R0 , #70	        ;First no. a	
+			  MOV R1 , #30          ;Second no. b
+Loop		  CMP R0 , R1           ;Comparing both numbers a-b
+              IT EQ                 ;If a=b
+			  BEQ Stop              ;Conditional branch to stop
+              IT HI			        ;C set and Z clear
+			  SUBHI R0 , R0 , R1 	;a=a-b		  
+			  IT MI                 ;N conditional flag set
+			  SUBMI R1 , R1 , R0    ;b=b-a
+			  B Loop			  
+Stop		  B Stop
+        endfunc
+      end
 
