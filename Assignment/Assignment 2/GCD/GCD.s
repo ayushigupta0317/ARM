5bf1ce2ee9bbdd940990895b3a04cc149677fffa
ayushigupta0317 <44375350+ayushigupta0317@users.noreply.github.com>
2018-10-22 23:31:48 +0530

    Update GCD.s

--- Assignment/Assignment 2/GCD/GCD.s
+++ Assignment/Assignment 2/GCD/GCD.s
@@ -3,15 +3,15 @@
 	 ENTRY 
 __main  function
 	          MOV R0 , #70	        ;First no. a	
-			  MOV R1 , #30          ;Second no. b
+		  MOV R1 , #30          ;Second no. b
 Loop		  CMP R0 , R1           ;Comparing both numbers a-b
-              IT EQ                 ;If a=b
-			  BEQ Stop              ;Conditional branch to stop
-              IT HI			        ;C set and Z clear
-			  SUBHI R0 , R0 , R1 	;a=a-b		  
-			  IT MI                 ;N conditional flag set
-			  SUBMI R1 , R1 , R0    ;b=b-a
-			  B Loop			  
+                  IT EQ                 ;If a=b
+		  BEQ Stop              ;Conditional branch to stop
+                  IT HI			        ;C set and Z clear
+		  SUBHI R0 , R0 , R1 	;a=a-b		  
+		  IT MI                 ;N conditional flag set
+		  SUBMI R1 , R1 , R0    ;b=b-a
+		  B Loop			  
 Stop		  B Stop
         endfunc
       end
