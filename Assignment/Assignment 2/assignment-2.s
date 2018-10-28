74631292fe9f3fd1d85de4a2177ef7cae8ab70a9
ayushigupta0317 <44375350+ayushigupta0317@users.noreply.github.com>
2018-10-28 12:03:41 +0530

    Update assignment-2.s

--- Assignment/Assignment 2/assignment-2.s
+++ Assignment/Assignment 2/assignment-2.s
@@ -8,8 +8,8 @@ code_1.s(14): error: A1619E: Specified condition is not consistent with previous
 
 2.)  " LT " means If-Then-Else block is conditional.
 
-3.)  In CODE-1, Then instructions and Else instruction is written without any condition, i.e. no condition used in ITTTE block.
+3.)  In CODE-1, Then instructions and Else instruction is written without any condition, i.e. no conditional instructions used in ITTTE block.
 
 4.)  In CODE-2, then block use LT conditional instructions whereas else block uses GE conditional instruction. 
 
-5.)  As per rule of if-then-else, else condition must be inverse of then condition, here LT inverse of GE.
+5.)  As per rule of if-then-else, else condition must be inverse of then condition, here LT is inverse of GE.
